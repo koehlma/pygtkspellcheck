ba8a549b4406c9011042acbf72af314117152a73
koehlma <linuxmaxi@googlemail.com>
2012-04-08 18:47:41 +0200

    issue #1 fixed / setup now includes also language files

--- src/setup.py
+++ src/setup.py
@@ -1,4 +1,5 @@
 from distutils.core import setup
 setup(name='pygtkspellchecker',
       version='1.0',
-      packages=['gtkspellcheck', 'pylocale'])
+      packages=['gtkspellcheck', 'pylocale'],
+      package_data={'pylocale' : ['locales.db', 'locales/*/*/*']})
