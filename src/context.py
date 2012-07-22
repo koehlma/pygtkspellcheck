ed96e9c8f36424ed5520226a74a16a503e5254cc
Maximilian Köhl <linuxmaxi@googlemail.com>
2012-07-22 23:46:35 +0200

    improving and commenting context.py

--- src/context.py
+++ src/context.py
@@ -16,27 +16,45 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""context.py: Context class for multiplatform and multilanguage applications."""
+"""
+Context module for multiplatform and multilanguage applications. It uses monkey
+patching to support modules written for the standard library. Modules just use
+`logging.getLogger` and `gettext.translation` and this modules cares about
+setting the right handlers and levels for logging as well as the searching paths
+for translation files. It also provides a `os.path.get_data_path` function to
+let modules know where they could find their data.
+"""
 
+import gettext
+import logging
 import os
 import os.path
 import sys
-import gettext
-import logging
 
+# Public Objects
+__all__ = ['getLogger', 'get_logger', 'setLevels', 'set_levels', 'Gettext',
+           'translation', 'set_locale_dir', 'get_data_path']
+
+# Save the old `logging.Manager` and the old `gettext.translation`.
+logging._OldManager = logging.Manager
+gettext._old_translation = gettext.translation
+
+# New manager for logging which supports a setLevels method to set the same
+# levels to all loggers requested with `logging.getLogger`.
 class Manager(logging.Manager):
-    _Manager = logging.Manager
     def __init__(self, *args, **kwargs):
-        self._Manager.__init__(self, *args, **kwargs)
+        logging._OldManager.__init__(self, *args, **kwargs)
         self._current_level = logging.WARNING
     
     def getLogger(self, name):
         if name in self.loggerDict:
-            return self._Manager.getLogger(self, name)
+            return logging._OldManager.getLogger(self, name)
         else:
-            logger = self._Manager.getLogger(self, name)
-            logger.addHandler(logging.StreamHandler()) # FIXME: Windows TODO: should not be hardcoded
-            logger.setLevel(self._current_level) # TODO: should not be hardcoded
+            logger = logging._OldManager.getLogger(self, name)
+            # FIXME: Windows TODO: should not be hardcoded
+            logger.addHandler(logging.StreamHandler())
+            # TODO: should not be hardcoded
+            logger.setLevel(self._current_level) 
             return logger
     
     def setLevels(self, level):
@@ -54,66 +72,86 @@ logging.Logger.manager = _manager
 logging.manager = _manager
 logging.setLevels = _manager.setLevels
 
+getLogger = _manager.getLogger
+get_logger = getLogger # PEP 8
+setLevels = _manager.setLevels
+set_levels = setLevels # PEP 8
+
+# logging for the rest of the module
 logger = logging.getLogger(__name__)
 
-# Maybe not a good solution...
+
+# Monkey patch a `os.path.get_data_path` function to allow applications that
+# support this monkey patch locate the directory where static data is located.
+# The default behavior is to return the directory where the requesting source
+# file is located. If this is not possible because of a frozen environment
+# (py2exe for example) it returns the path where the exe or dll is located.
 def get_data_path(file_var):
     """
-    Allows any module to know where he is.
-    $file_var must be current module __file__
+    Allows any module to know where to find his data also for frozen
+    environments. The `file_var` must be the current module's `__file__`.
     """
     frozen = getattr(sys, 'frozen', '')
     if not frozen:
-        where_am_i = os.path.normpath(os.path.dirname(os.path.abspath(os.path.realpath(file_var))))
+        return os.path.normpath(os.path.dirname(os.path.abspath(os.path.realpath(file_var))))
     elif frozen in ('dll', 'console_exe', 'windows_exe'):
-        where_am_i = os.path.normpath(os.path.dirname(sys.executable))
-    return where_am_i
+        return os.path.normpath(os.path.dirname(sys.executable))
+    
 os.path.get_data_path = get_data_path
 
+# A new `gettext.translation` function that cares about the localedir for every
+# application that uses it. So a default path for all translation files could be
+# given. Also the `fallback` argument would be `True` for every translation.
 class Gettext():
-    _translation = gettext.translation
-    def __init__(self):
-        self._builder_used = False
-        self._root = os.path.get_data_path(__file__) 
-        self._default = 'default'
-        default_locale = {'linux' : '/usr/share/locale',
-                          'win' : os.path.join(self._root, 'l10n')}
-        self._locale_dir = default_locale['linux']
-        if sys.platform.startswith('win'):
-            self._locale_dir = default_locale['win']
-        if os.getenv('LANG') is None:
-            import locale
-            lang, enc = locale.getdefaultlocale()
-            os.environ['LANG'] = lang         
+    BUILDER_USED = False # Only one translation could use GtkBuilder
+    ROOT = get_data_path(__file__)
+    DEFAULT_LOCALE = 'default' # if domain is `None`. This is not compatible
+                               # with the python standard library. So then
+                               # context.py is required.
+    DEFAULT_DIRS = {'linux' : '/usr/share/locale',
+                    'windows' : os.path.join(ROOT, 'l10n')}
+    LOCALE_DIR = DEFAULT_DIRS['linux']
+    if sys.platform.startswith('win'):
+        LOCALE_DIR = DEFAULT_DIRS['windows']   
         
     def translation(self, domain=None, localedir=None, languages=None, class_=None,
                     fallback=False, codeset=None, builder=False):
-        domain = self._default if domain is None else domain
+        domain = Gettext.DEFAULT_LOCALE if domain is None else domain
         logger.debug('requesting translation for domain "{}"'.format(domain))
         # Only one application can be 'Builder' (Application that uses GtkBuilder)
-        if builder and not self._builder_used and sys.platform.startswith('win'):
+        if builder and not Gettext.BUILDER_USED and sys.platform.startswith('win'):
             self._builder_used = True
             # Glade file translations
             try:
                 import ctypes
                 libintl = ctypes.cdll.LoadLibrary('intl.dll')
-                libintl.bindtextdomain(domain, self._locale_dir)
-                libintl.bind_textdomain_codeset(domain, 'UTF-8')
+                libintl.bindtextdomain(domain, Gettext.LOCALE_DIR)
+                libintl.bind_textdomain_codeset(domain, 'utf-8')
             except:
                 logger.error('Error loading translations into Glade file.')
-        if gettext.find(domain, self._locale_dir):
-            return gettext.old_translation(domain, self._locale_dir, languages, class_,
+        # search in default locale directory first
+        if gettext.find(domain, Gettext.LOCALE_DIR):
+            return gettext._old_translation(domain, Gettext.LOCALE_DIR, languages, class_,
+                                            True, codeset)
+        # then in given locale directory
+        return gettext._old_translation(domain, localedir, languages, class_,
                                         True, codeset)
-        return gettext.old_translation(domain, localedir, languages, class_,
-                                    True, codeset)
     
     def set_locale_dir(self, locale_dir):
-        self._locale_dir = locale_dir
+        Gettext.LOCALE_DIR = locale_dir
 
 # Do some monkey patching, so applications and libraries could just use the
 # python standard way `gettext.translation` and must not carry context.py to
 # work correctly with it.
-gettext.old_translation = gettext.translation  
 _gettext = Gettext()
 gettext.translation = _gettext.translation
 gettext.set_locale_dir = _gettext.set_locale_dir
+
+translation = _gettext.translation
+set_locale_dir = _gettext.set_locale_dir
+
+# setting LANG environment variable if not already set
+if os.getenv('LANG') is None:
+    import locale
+    lang, enc = locale.getdefaultlocale()
+    os.environ['LANG'] = lang
