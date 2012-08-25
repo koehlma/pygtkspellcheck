c1a5bdb7f7abbcef7eeb26d4c0b3bc719f9e2779
Maximilian Köhl <linuxmaxi@googlemail.com>
2012-08-25 20:54:58 +0200

    oxt_extract command line utility

---
+++ utils/oxt_extract_dictionaries.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#
+# Copyright (C) 2012, Maximilian Köhl <linuxmaxi@googlemail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+import argparse
+
+sys.modules['gtk'] = None
+from gtkspellcheck.oxt_extract import extract
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('extension', nargs='+',
+                        help='extension to extract')
+    parser.add_argument('target',
+                        help='target directory')
+    
+    arguments = parser.parse_args()
+    for extension in arguments.extension:
+        extract(extension, arguments.target)
